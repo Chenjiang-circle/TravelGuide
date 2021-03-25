11a18cc0bb8260504cdbdcc1c671312bfe92e9f3
Circle <1455075085@qq.com>
2021-03-25 18:55:56 +0800

    完成个人页面登录之后的渲染

--- miniprogram/pages/userCenter/userCenter.js
+++ miniprogram/pages/userCenter/userCenter.js
@@ -5,6 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    userInfo: {},
     menuitems: [{
         text: '个人资料',
         url: '#',
@@ -40,7 +41,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var app = getApp();
+    this.setData({
+      userInfo: app.globalData.userInfo
+    })
   },
 
   /**
